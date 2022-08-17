42bbd7eb190b472afd0d3aa7f18c6207a9a8a953
Erik Svensson <eosvensson@gmail.com>
2022-08-17 12:42:02 +0200

    Beer was converted to a class

--- src/main/java/com/example/beerclient/model/Beer.java
+++ src/main/java/com/example/beerclient/model/Beer.java
@@ -1,24 +1,32 @@
 package com.example.beerclient.model;
 
+import lombok.*;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record Beer(
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer implements Serializable {
         @Null
-        UUID id,
+        UUID id;
         @NotBlank
-        String beerName,
+        String beerName;
         @NotNull
-        BeerStyle beerStyle,
-        String upc,
-        Integer quantityOnHand,
-        BigDecimal price,
-        Integer version,
-        LocalDateTime createdDate,
-        LocalDateTime lastModifiedDate
-) {
+        BeerStyle beerStyle;
+        String upc;
+        Integer quantityOnHand;
+        BigDecimal price;
+        Integer version;
+        LocalDateTime createdDate;
+        LocalDateTime lastModifiedDate;
+
 }
